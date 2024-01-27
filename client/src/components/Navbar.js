de55531a3e754739eabbf635b8b3ed9b8e2d5596
Andrea Kastratovic <andrea.kastratovic@gmail.com>
2024-01-27 14:50:51 +0100

    instaliran react router dom

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from '../assets/logo.png'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="levaStrana">
+            <img src={Logo} />
+
+        </div>
+        <div className="desnaStrana">
+           
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
