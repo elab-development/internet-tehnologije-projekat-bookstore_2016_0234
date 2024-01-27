df79d55168c7a9349c158d3d6207fdee3d361c53
merionization <marijamgmmarinkovic@gmail.com>
2024-01-27 16:50:23 +0100

    Home js i css dodati

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+//import {Link} from 'react-router-dom'
+import BackgroundImage from '../assets/homebackground.jpg'
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home" style={{backgroundImage: `url(${BackgroundImage})`, opacity:0.8 }}>
+      <div className="headerContainer">
+        <h1>BOOKS FOR EVERYONE</h1>
+        <p>Find all your favourite books in one place</p>
+        {/*<Link to="/shop">
+          <button>
+          DISCOVER MORE
+          </button>
+        </Link>
+          */}
+      </div>
+    </div>
   )
 }
 
