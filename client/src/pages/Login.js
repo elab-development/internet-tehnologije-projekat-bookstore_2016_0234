88a13b31db0bbd55f2b56d6b8fcffd150c980437
merionization <marijamgmmarinkovic@gmail.com>
2024-01-27 17:52:00 +0100

    Dodate slike i promenjen login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import '../styles/Login.css'; 
+import React, {useState} from 'react';
+import '../styles/Login.css';
+import LoginImage from '../assets/homebackground.jpg'; 
 
 function Login  () {
+    // State to manage form inputs
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Function to handle login
+  //const handleLogin = (event) => {
+    //event.preventDefault();
+    // Perform your login logic here
+    //console.log('Logging in with username:', username, 'and password:', password);
+    // You can make an API call or perform any authentication logic here
+  //};
   return (
-    <div className="home">
-      <div className="headerContainer">
-        <h1>Login</h1>
-        <p>Welcome back! Please log in to your account.</p>
-        <form>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" />
-
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
-
+    <div className="login">
+      <div className="leftSide">
+        <form >
+          <label htmlFor="username">Username</label>
+          <input
+            name="username"
+            placeholder="Enter username..."
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            placeholder="Enter password..."
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button type="submit">Log In</button>
         </form>
       </div>
+      <div className="rightSide" style={{ backgroundImage: `url(${LoginImage})` }}></div>
     </div>
   );
 };
