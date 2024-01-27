559b06ca1254f899a8d9b7a724e1b7591817f3d2
Andrea Kastratovic <andrea.kastratovic@gmail.com>
2024-01-27 17:28:23 +0100

    ikonica za kart dodata u navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Logo from '../assets/logo.png'
 import {Link} from 'react-router-dom'
 import '../styles/navbar.css'
+import { FiShoppingCart } from "react-icons/fi";
 
 function Navbar() {
   return (
@@ -14,7 +15,7 @@ function Navbar() {
             <Link to="/">HOME</Link>
             <Link to="/shop">SHOP</Link>
             <Link to="/contact">CONTACT</Link>
-            
+            <Link to="/cart"><FiShoppingCart size={30} /></Link>
         </div>
     </div>
   )
